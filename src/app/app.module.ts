ca059c5d679b518c9bae610561956e18f4e5afbc
Kassiane <k.pasquali-andrade@beaconinside.com>
2019-12-04 21:56:59 +0100

    Creating error component and adding style to error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { VehiclesComponent } from './components/vehicles/vehicles.component';
 import { SelectCarComponent } from './navigation/select-car/select-car.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiModule } from './api/api.module';
+import { ErrorComponent } from './component/error/error.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ApiModule } from './api/api.module';
     ModelsComponent,
     VehiclesComponent,
     SelectCarComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
